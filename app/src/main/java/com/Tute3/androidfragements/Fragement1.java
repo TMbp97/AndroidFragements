37b8bfb6ff6d3a37e31e425365bdf941b5c85521
BPabey <buddimalp@gmail.com>
2020-03-25 19:22:00 +0530

    Initial commit

---
+++ app/src/main/java/com/Tute3/androidfragements/Fragement1.java
@@ -0,0 +1,18 @@
+package com.Tute3.androidfragements;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragement1 extends Fragment {
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_fragement1, container, false);
+    }
+}
